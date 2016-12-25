96ca7812bd230f9160c89f6575d718d8e604d84a
Your Name <you@example.com>
2016-12-25 09:04:28 +0800

    2

---
+++ ex3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+print "I will now count my chickens:"
+# 计算母鸡和公鸡各有多少只
+print "Hens", 25+30/6
+print "Roosters", 100-25*3%4
+# 计算蛋的数量
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6
+# 这个比较是否正确
+print "Is it true that 3+2<5-7?"
+
+print 3+2<5-7
+
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+# 找出错误的地方
+print "Oh, than's why it's False."
+
+print "How about some more."
+# 比较是否正确
+print "Is it greater?", 5>-2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<=-2
